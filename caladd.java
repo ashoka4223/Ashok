8639e0e51deb660b0a436e9f0d8e875fdeafeb66
ashok <ashokkumarchoudhary4223@gmail.com>
2022-10-05 11:14:16 +0530

    new file

---
+++ caladd.java
@@ -0,0 +1,8 @@
+public class caladd {
+    public static void main(string[] args){
+        int a =4;
+        int b = 7;
+        int sum = a + b;
+        system.out.println("The sum is " sum);
+    }
+}
---
+++ helloworld.java
@@ -0,0 +1,10 @@
+public class helloworld{
+   public static void main(String[] args)
+    {
+      int a,b,sum;
+      a=4;
+      b=5;
+      sum = (a+b);
+      System.out.println( sum);  
+    }
+}
